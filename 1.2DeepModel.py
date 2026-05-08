6d04c33d72c63ace70766395702fea1e57bb2a15
Nilesh Parmar <nileshpar835@gmail.com>
2026-05-08 10:56:07 +0530

    mnist update

--- 1.2DeepModel.py
+++ 1.2DeepModel.py
@@ -102,4 +102,33 @@ def train_model(model, train_loader, loss_fn, optimizer, device):
             running_loss = 0.0
 
 # Train model
-train_model(model, train_loader, loss_fn, optimizer, device)
+# train_model(model, train_loader, loss_fn, optimizer, device)
+
+def evaluate(model , test_loader , device):
+    model.eval()
+    correct=0
+    total=0
+
+    with torch.no_grad():
+        for inputs, targets in test_loader:
+            inputs,targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            _,predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+    return 100. * correct/total
+
+num_epochs=10
+for epoch in range(num_epochs):
+    print(f'\n Epoch:{epoch+1}')
+    train_model(model , train_loader , loss_fn , optimizer , device)
+    accuracy = evaluate(model , test_loader , device)
+    print(f'Test Accuracy:{accuracy:.2f}')
+
+
+
+
+
+
+
+
